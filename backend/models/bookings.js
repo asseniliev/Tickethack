8db96ae576e2f14d671ca5029a3f4d68774fdc98
Asen Iliev <assen.s.iliev@gmail.com>
2023-01-24 16:23:41 +0100

    put booking on the right place

--- backend/models/bookings.js
+++ backend/models/bookings.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-var express = require("express");
-const { route } = require("../app.js");
-var router = express.Router();
-
-const Bookings = require('../models/bookings.js');
-
-router.get('/', (req, res) => {
-  Bookings.find()
-       .then((data) => res.json({data}));
-});
-
-router.post('/', (req, res) => {
-  const newBooking = new Bookings({
-    selectedTrip: req.selectedTrip,
-    buyingPrice: req.buyingPrice
-  });
-
-  newBooking.save().then(() => {});
-
-});
-
-
-=======
 const mongoose = require("mongoose");
 
 const bookingSchema = mongoose.Schema({
@@ -35,4 +11,3 @@ const bookingSchema = mongoose.Schema({
 const Booking = mongoose.model("bookings", bookingSchema);
 
 module.exports = Booking;
->>>>>>> e9887b70bcbfb7dc761c86b019ad5a35dbb6a90d
