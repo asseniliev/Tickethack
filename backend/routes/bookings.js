8ec6a7b46e21f75560a3a8d6ecb0ee9bdfd03523
Asen Iliev <assen.s.iliev@gmail.com>
2023-01-24 17:15:39 +0100

    resolve bugs in the routes

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-<<<<<<< HEAD
 const Booking = require('../models/bookings.js');
 
+
+//Select all bookings
 router.get('/', (req, res) => {
   Booking.find()
        .then((data) => res.json({data}));
@@ -11,32 +12,17 @@ router.get('/', (req, res) => {
 
 //63cfba6cd54d01ebd8051603
 
+//Post a new booking in the database
 router.post('/', (req, res) => {
   const newBooking = new Booking({
     selectedTrip: req.body.selectedTrip,
     buyingPrice: req.body.buyingPrice
   });
 
-  console.log(newBooking);
+  console.log(req.body.buyingPrice);
 
   newBooking.save().then((data) => {res.json(data)});
 
 });
 
 module.exports = router;
-=======
-const Bookings = require("../models/bookings.js");
-
-router.get("/", (req, res) => {
-	Bookings.find().then((data) => res.json({ data }));
-});
-
-router.post("/", (req, res) => {
-	const newBooking = new Bookings({
-		selectedTrip: req.selectedTrip,
-		buyingPrice: req.buyingPrice,
-	});
-
-	newBooking.save().then(() => {});
-});
->>>>>>> 7aa2418782ceb33bff0315f687ca3b612478b8a3
