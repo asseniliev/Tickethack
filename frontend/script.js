d6b356c295d6142b082e5d7d456e64b927dcbdf3
Jason RICOU <jason.ricou@gmail.com>
2023-01-24 17:09:01 +0100

    commit

---
+++ frontend/script.js
@@ -0,0 +1,25 @@
+
+/* GET
+fetch('http://localhost:3000/bitcoin')
+.then(response => response.json())
+.then(data => {
+  console.log(data.bitcoinInfos);
+});
+*/
+
+
+/* POST
+fetch('http://localhost:3000/save', {
+
+    method: 'POST',
+
+    headers: { 'Content-Type': 'application/json' },
+
+    body: JSON.stringify(data)
+
+})
+ .then(response => response.json())
+ .then(data => {
+   console.log(data);
+ });
+*/
