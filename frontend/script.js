886c0419da9acf0ae84f6c0aae9994844e409410
Jason RICOU <jason.ricou@gmail.com>
2023-01-25 10:50:50 +0100

    html

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,8 @@
+//Temporary cart
 
-/* GET
-fetch('http://localhost:3000/bitcoin')
-.then(response => response.json())
-.then(data => {
-  console.log(data.bitcoinInfos);
-});
-*/
+let cart = []
+
+// GET envoyant lieu de depart,d'arrivée et une date, recupère des trajets dans trips
 
 
-/* POST
-fetch('http://localhost:3000/save', {
 
-    method: 'POST',
-
-    headers: { 'Content-Type': 'application/json' },
-
-    body: JSON.stringify(data)
-
-})
- .then(response => response.json())
- .then(data => {
-   console.log(data);
- });
-*/
